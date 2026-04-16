19c174cc33e53819b82280e50581095553448865
Renwang-Huang <RenwangHuangX@gmail.com>
2026-04-17 02:31:59 +0800

    修复编译警告

--- rog_map/include/utils/common_lib.hpp
+++ rog_map/include/utils/common_lib.hpp
@@ -1,25 +1,25 @@
 /**
-* This file is part of ROG-Map
-*
-* Copyright 2024 Yunfan REN, MaRS Lab, University of Hong Kong, <mars.hku.hk>
-* Developed by Yunfan REN <renyf at connect dot hku dot hk>
-* for more information see <https://github.com/hku-mars/ROG-Map>.
-* If you use this code, please cite the respective publications as
-* listed on the above website.
-*
-* ROG-Map is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ROG-Map is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with ROG-Map. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of ROG-Map
+ *
+ * Copyright 2024 Yunfan REN, MaRS Lab, University of Hong Kong, <mars.hku.hk>
+ * Developed by Yunfan REN <renyf at connect dot hku dot hk>
+ * for more information see <https://github.com/hku-mars/ROG-Map>.
+ * If you use this code, please cite the respective publications as
+ * listed on the above website.
+ *
+ * ROG-Map is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ROG-Map is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ROG-Map. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
@@ -36,9 +36,10 @@
 #include <vector>
 #include <Eigen/Eigen>
 
-
-#define DEBUG_FILE_DIR(name) (string(string(ROOT_DIR) + "log/"+name))
-#define PCD_FILE_DIR(name) (string(string(ROOT_DIR) + "pcd/"+name))
+// Cleaned up duplicate macros and added std:: prefix for safety
+#define DEBUG_FILE_DIR(name) (std::string(std::string(ROOT_DIR) + "log/" + name))
+#define PCD_FILE_DIR(name) (std::string(std::string(ROOT_DIR) + "pcd/" + name))
+#define SIGN(x) ((x > 0) - (x < 0))
 
 namespace rog_map {
     typedef double decimal_t;
@@ -46,6 +47,7 @@ namespace rog_map {
     using Vec3f = Eigen::Matrix<decimal_t, 3, 1>;
     using Quatf = Eigen::Quaterniond;
     using Pose = std::pair<Vec3f, Eigen::Quaterniond>;
+    
     template<typename T>
     using vec_E = std::vector<T, Eigen::aligned_allocator<T>>;
     using vec_Vec3i = vec_E<Vec3i>;
@@ -55,7 +57,6 @@ namespace rog_map {
     using std::string;
     using std::cout;
     using std::endl;
-
 
     constexpr int AXIS_X = 0;
     constexpr int AXIS_Y = 1;
@@ -70,7 +71,6 @@ namespace rog_map {
 
     typedef pcl::PointXYZINormal PclPoint;
     typedef pcl::PointCloud<PclPoint> PointCloud;
-
 
     enum GridType {
         UNDEFINED = 0,
@@ -88,10 +88,11 @@ namespace rog_map {
                                                       "KNOWN_FREE",
                                                       "FRONTIER"};
 
-
-#define SIGN(x) ((x > 0) - (x < 0))
-#define DEBUG_FILE_DIR(name) (string(string(ROOT_DIR) + "log/"+name))
-#define PCD_FILE_DIR(name) (string(string(ROOT_DIR) + "pcd/"+name))
+    // Replaced error-prone SIGN macro with a safe inline template
+    template <typename T>
+    inline int sign(T val) {
+        return (T(0) < val) - (val < T(0));
+    }
 
     template<typename T>
     std::ostream &operator<<(std::ostream &out, const std::vector<T> &v) {
@@ -116,7 +117,7 @@ namespace rog_map {
 
 
     template<typename Scalar_t>
-    static Eigen::Matrix<Scalar_t, 3, 1> quaternion_to_ypr(const Eigen::Quaternion<Scalar_t> &q_) {
+    inline Eigen::Matrix<Scalar_t, 3, 1> quaternion_to_ypr(const Eigen::Quaternion<Scalar_t> &q_) {
         Eigen::Quaternion<Scalar_t> q = q_.normalized();
 
         Eigen::Matrix<Scalar_t, 3, 1> ypr;
@@ -128,14 +129,12 @@ namespace rog_map {
     }
 
     template<typename Scalar_t>
-    static Scalar_t get_yaw_from_quaternion(const Eigen::Quaternion<Scalar_t> &q) {
+    inline Scalar_t get_yaw_from_quaternion(const Eigen::Quaternion<Scalar_t> &q) {
         return quaternion_to_ypr(q)(0);
     }
 
 
-
-
-    static double computePathLength(const vec_E<Vec3f> &path) {
+    inline double computePathLength(const vec_E<Vec3f> &path) {
         if (path.size() < 2) {
             return 0.0;
         }
@@ -146,7 +145,7 @@ namespace rog_map {
         return len;
     }
 
-    static Vec3f lineBoxIntersectPoint(const Vec3f &pt, const Vec3f &pos,
+    inline Vec3f lineBoxIntersectPoint(const Vec3f &pt, const Vec3f &pos,
                                        const Vec3f &box_min, const Vec3f &box_max) {
         Eigen::Vector3d diff = pt - pos;
         Eigen::Vector3d max_tc = box_max - pos;
@@ -170,26 +169,26 @@ namespace rog_map {
         return pos + (min_t - 1e-3) * diff;
     }
 
-    static bool GetIntersection(float fDst1, float fDst2, Vec3f P1, Vec3f P2, Vec3f &Hit) {
+    inline bool GetIntersection(float fDst1, float fDst2, Vec3f P1, Vec3f P2, Vec3f &Hit) {
         if ((fDst1 * fDst2) >= 0.0f) return false;
         if (fDst1 == fDst2) return false;
         Hit = P1 + (P2 - P1) * (-fDst1 / (fDst2 - fDst1));
         return true;
     }
 
-    static bool InBox(Vec3f Hit, Vec3f B1, Vec3f B2, const int Axis) {
+    inline bool InBox(Vec3f Hit, Vec3f B1, Vec3f B2, const int Axis) {
         if (Axis == 1 && Hit.z() > B1.z() && Hit.z() < B2.z() && Hit.y() > B1.y() && Hit.y() < B2.y()) return true;
         if (Axis == 2 && Hit.z() > B1.z() && Hit.z() < B2.z() && Hit.x() > B1.x() && Hit.x() < B2.x()) return true;
         if (Axis == 3 && Hit.x() > B1.x() && Hit.x() < B2.x() && Hit.y() > B1.y() && Hit.y() < B2.y()) return true;
         return false;
     }
 
-//The box in this article is Axis-Aligned and so can be defined by only two 3D points:
-// B1 - the smallest values of X, Y, Z
-//        B2 - the largest values of X, Y, Z
-// returns true if line (L1, L2) intersects with the box (B1, B2)
-// returns intersection point in Hit
-    static bool lineIntersectBox(Vec3f L1, Vec3f L2, Vec3f B1, Vec3f B2, Vec3f &Hit) {
+    //The box in this article is Axis-Aligned and so can be defined by only two 3D points:
+    // B1 - the smallest values of X, Y, Z
+    //        B2 - the largest values of X, Y, Z
+    // returns true if line (L1, L2) intersects with the box (B1, B2)
+    // returns intersection point in Hit
+    inline bool lineIntersectBox(Vec3f L1, Vec3f L2, Vec3f B1, Vec3f B2, Vec3f &Hit) {
         if (L2.x() < B1.x() && L1.x() < B1.x()) return false;
         if (L2.x() > B2.x() && L1.x() > B2.x()) return false;
         if (L2.y() < B1.y() && L1.y() < B1.y()) return false;
@@ -198,12 +197,12 @@ namespace rog_map {
         if (L2.z() > B2.z() && L1.z() > B2.z()) return false;
 
         // inside box seems intersect
-//        if (L1.x() > B1.x() && L1.x() < B2.x() &&
-//            L1.y() > B1.y() && L1.y() < B2.y() &&
-//            L1.z() > B1.z() && L1.z() < B2.z()) {
-//            Hit = L1;
-//            return true;
-//        }
+        // if (L1.x() > B1.x() && L1.x() < B2.x() &&
+        //     L1.y() > B1.y() && L1.y() < B2.y() &&
+        //     L1.z() > B1.z() && L1.z() < B2.z()) {
+        //     Hit = L1;
+        //     return true;
+        // }
 
         if ((GetIntersection(L1.x() - B1.x(), L2.x() - B1.x(), L1, L2, Hit) && InBox(Hit, B1, B2, 1))
             || (GetIntersection(L1.y() - B1.y(), L2.y() - B1.y(), L1, L2, Hit) && InBox(Hit, B1, B2, 2))
--- rog_map/include/utils/visual_utils.hpp
+++ rog_map/include/utils/visual_utils.hpp
@@ -1,25 +1,25 @@
 /**
-* This file is part of ROG-Map
-*
-* Copyright 2024 Yunfan REN, MaRS Lab, University of Hong Kong, <mars.hku.hk>
-* Developed by Yunfan REN <renyf at connect dot hku dot hk>
-* for more information see <https://github.com/hku-mars/ROG-Map>.
-* If you use this code, please cite the respective publications as
-* listed on the above website.
-*
-* ROG-Map is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ROG-Map is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with ROG-Map. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of ROG-Map
+ *
+ * Copyright 2024 Yunfan REN, MaRS Lab, University of Hong Kong, <mars.hku.hk>
+ * Developed by Yunfan REN <renyf at connect dot hku dot hk>
+ * for more information see <https://github.com/hku-mars/ROG-Map>.
+ * If you use this code, please cite the respective publications as
+ * listed on the above website.
+ *
+ * ROG-Map is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ROG-Map is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ROG-Map. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
@@ -46,7 +46,7 @@ namespace rog_map {
             b = static_cast<double>(_b) / 255.0;
         }
 
-        Color(Color c, double alpha) {
+        Color(const Color& c, double alpha) {
             r = c.r;
             g = c.g;
             b = c.b;
@@ -66,54 +66,42 @@ namespace rog_map {
             a = alpha;
         }
 
-        static const Color White() { return Color(1.0, 1.0, 1.0); }
-
-        static const Color Black() { return Color(0.0, 0.0, 0.0); }
-
-        static const Color Gray() { return Color(0.5, 0.5, 0.5); }
-
-        static const Color Red() { return Color(1.0, 0.0, 0.0); }
-
-        static const Color Green() { return Color(0.0, 0.96, 0.0); }
-
-        static const Color Blue() { return Color(0.0, 0.0, 1.0); }
-
-        static const Color SteelBlue() { return Color(0.4, 0.7, 1.0); }
-
-        static const Color Yellow() { return Color(1.0, 1.0, 0.0); }
-
+        static Color White() { return Color(1.0, 1.0, 1.0); }
+        static Color Black() { return Color(0.0, 0.0, 0.0); }
+        static Color Gray() { return Color(0.5, 0.5, 0.5); }
+        static Color Red() { return Color(1.0, 0.0, 0.0); }
+        static Color Green() { return Color(0.0, 0.96, 0.0); }
+        static Color Blue() { return Color(0.0, 0.0, 1.0); }
+        static Color SteelBlue() { return Color(0.4, 0.7, 1.0); }
+        static Color Yellow() { return Color(1.0, 1.0, 0.0); }
         static Color Orange() { return Color(1.0, 0.5, 0.0); }
-
-        static const Color Purple() { return Color(0.5, 0.0, 1.0); }
-
-        static const Color Chartreuse() { return Color(0.5, 1.0, 0.0); }
-
-        static const Color Teal() { return Color(0.0, 1.0, 1.0); }
-
-        static const Color Pink() { return Color(1.0, 0.0, 0.5); }
+        static Color Purple() { return Color(0.5, 0.0, 1.0); }
+        static Color Chartreuse() { return Color(0.5, 1.0, 0.0); }
+        static Color Teal() { return Color(0.0, 1.0, 1.0); }
+        static Color Pink() { return Color(1.0, 0.0, 0.5); }
     };
 
 
     /* Type A, directly publish marker in publisher */
-    static void visualizeText(const rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr &pub,
+    inline void visualizeText(const rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr &pub,
                               const std::string &ns,
                               const std::string &text,
                               const Vec3f &position,
                               const Color &c,
-                              const double &size,
-                              const int &id = -1) {
+                              double size,
+                              int id = -1) {
         visualization_msgs::msg::Marker marker;
         // marker.header.frame_id = "world";
         marker.header.frame_id = "camera_init";
         marker.header.stamp = rclcpp::Clock().now(); 
         marker.action = visualization_msgs::msg::Marker::ADD;
         marker.pose.orientation.w = 1.0;
-        marker.ns = ns.c_str();
+        marker.ns = ns;
         if (id >= 0) {
             marker.id = id;
         } else {
-            static int id = 0;
-            marker.id = id++;
+            static int static_id = 0;
+            marker.id = static_id++;
         }
         marker.type = visualization_msgs::msg::Marker::TEXT_VIEW_FACING;
         marker.scale.z = size;
@@ -126,14 +114,15 @@ namespace rog_map {
         visualization_msgs::msg::MarkerArray arr;
         arr.markers.push_back(marker);
         pub->publish(arr); 
-    };
+    }
 
-    static void visualizePoint(const rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr &pub_,
+    inline void visualizePoint(const rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr &pub_,
                                const Vec3f &pt,
                                Color color = Color::Pink(),
-                               std::string ns = "pt",
-                               double size = 0.1, int id = -1,
-                               const bool &print_ns = true) {
+                               const std::string& ns = "pt",
+                               double size = 0.1, 
+                               int id = -1,
+                               bool print_ns = true) {
         visualization_msgs::msg::MarkerArray mkr_arr;
         visualization_msgs::msg::Marker marker_ball;
         static int cnt = 0;
@@ -143,7 +132,7 @@ namespace rog_map {
         }
         marker_ball.header.frame_id = "camera_init";
         marker_ball.header.stamp = rclcpp::Clock().now();
-        marker_ball.ns = ns.c_str();
+        marker_ball.ns = ns;
         marker_ball.id = id >= 0 ? id : cnt++;
         marker_ball.action = visualization_msgs::msg::Marker::ADD;
         marker_ball.pose.orientation.w = 1.0;
@@ -161,7 +150,7 @@ namespace rog_map {
         marker_ball.pose.position = p;
         mkr_arr.markers.push_back(marker_ball);
 
-        // add test
+        // add text
         if (print_ns) {
             visualization_msgs::msg::Marker marker;
             // marker.header.frame_id = "world";
@@ -173,8 +162,8 @@ namespace rog_map {
             if (id >= 0) {
                 marker.id = id;
             } else {
-                static int id = 0;
-                marker.id = id++;
+                static int static_id = 0;
+                marker.id = static_id++;
             }
             marker.type = visualization_msgs::msg::Marker::TEXT_VIEW_FACING;
             marker.scale.z = 0.6;
@@ -190,19 +179,21 @@ namespace rog_map {
         pub_->publish(mkr_arr); 
     }
 
-    static void visualizeBoundingBox(const rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr &pub,
+    inline void visualizeBoundingBox(const rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr &pub,
                                      const Vec3f &box_min,
                                      const Vec3f &box_max,
-                                     const string &ns,
+                                     const std::string &ns,
                                      const Color &color,
-                                     const double &size_x = 0.1,
-                                     const double &alpha = 1.0,
-                                     const bool &print_ns = true) {
-        Vec3f size = (box_max - box_min) / 2;
-        Vec3f vis_pos_world = (box_min + box_max) / 2;
+                                     double size_x = 0.1,
+                                     double alpha = 1.0,
+                                     bool print_ns = true) {
+        (void)print_ns; // Suppress unused parameter warning
+        
+        Vec3f size = (box_max - box_min) / 2.0;
+        Vec3f vis_pos_world = (box_min + box_max) / 2.0;
         double width = size.x();
         double length = size.y();
-        double hight = size.z();
+        double height = size.z();
         visualization_msgs::msg::MarkerArray mkrarr;
         //Publish Bounding box
         int id = 0;
@@ -224,28 +215,28 @@ namespace rog_map {
         //vis_pos_world是目标物的坐标
         p[0].x = vis_pos_world(0) - width;
         p[0].y = vis_pos_world(1) + length;
-        p[0].z = vis_pos_world(2) + hight;
+        p[0].z = vis_pos_world(2) + height;
         p[1].x = vis_pos_world(0) - width;
         p[1].y = vis_pos_world(1) - length;
-        p[1].z = vis_pos_world(2) + hight;
+        p[1].z = vis_pos_world(2) + height;
         p[2].x = vis_pos_world(0) - width;
         p[2].y = vis_pos_world(1) - length;
-        p[2].z = vis_pos_world(2) - hight;
+        p[2].z = vis_pos_world(2) - height;
         p[3].x = vis_pos_world(0) - width;
         p[3].y = vis_pos_world(1) + length;
-        p[3].z = vis_pos_world(2) - hight;
+        p[3].z = vis_pos_world(2) - height;
         p[4].x = vis_pos_world(0) + width;
         p[4].y = vis_pos_world(1) + length;
-        p[4].z = vis_pos_world(2) - hight;
+        p[4].z = vis_pos_world(2) - height;
         p[5].x = vis_pos_world(0) + width;
         p[5].y = vis_pos_world(1) - length;
-        p[5].z = vis_pos_world(2) - hight;
+        p[5].z = vis_pos_world(2) - height;
         p[6].x = vis_pos_world(0) + width;
         p[6].y = vis_pos_world(1) - length;
-        p[6].z = vis_pos_world(2) + hight;
+        p[6].z = vis_pos_world(2) + height;
         p[7].x = vis_pos_world(0) + width;
         p[7].y = vis_pos_world(1) + length;
-        p[7].z = vis_pos_world(2) + hight;
+        p[7].z = vis_pos_world(2) + height;
         //LINE_STRIP类型仅仅将line_strip.points中相邻的两个点相连，如0和1，1和2，2和3
         for (int i = 0; i < 8; i++) {
             line_strip.points.push_back(p[i]);
@@ -265,25 +256,25 @@ namespace rog_map {
     }
 
     /* Type B: Add marker to given marker_arr for later publish */
-    static void visualizeText(visualization_msgs::msg::MarkerArray &mkr_arr,
+    inline void visualizeText(visualization_msgs::msg::MarkerArray &mkr_arr,
                               const std::string &ns,
                               const std::string &text,
                               const Vec3f &position,
                               const Color &c = Color::White(),
-                              const double &size = 0.6,
-                              const int &id = -1) {
+                              double size = 0.6,
+                              int id = -1) {
         visualization_msgs::msg::Marker marker;
         // marker.header.frame_id = "world";
         marker.header.frame_id = "camera_init";
         marker.header.stamp = rclcpp::Clock().now();
         marker.action = visualization_msgs::msg::Marker::ADD;
         marker.pose.orientation.w = 1.0;
-        marker.ns = ns.c_str();
+        marker.ns = ns;
         if (id >= 0) {
             marker.id = id;
         } else {
-            static int id = 0;
-            marker.id = id++;
+            static int static_id = 0;
+            marker.id = static_id++;
         }
         marker.type = visualization_msgs::msg::Marker::TEXT_VIEW_FACING;
         marker.scale.z = size;
@@ -294,14 +285,15 @@ namespace rog_map {
         marker.pose.position.z = position.z();
         marker.pose.orientation.w = 1.0;
         mkr_arr.markers.push_back(marker);
-    };
+    }
 
-    static void visualizePoint(visualization_msgs::msg::MarkerArray &mkr_arr,
+    inline void visualizePoint(visualization_msgs::msg::MarkerArray &mkr_arr,
                                const Vec3f &pt,
                                Color color = Color::Pink(),
-                               std::string ns = "pt",
-                               double size = 0.1, int id = -1,
-                               const bool &print_ns = true) {
+                               const std::string& ns = "pt",
+                               double size = 0.1, 
+                               int id = -1,
+                               bool print_ns = true) {
         visualization_msgs::msg::Marker marker_ball;
         static int cnt = 0;
         Vec3f cur_pos = pt;
@@ -311,7 +303,7 @@ namespace rog_map {
         // marker_ball.header.frame_id = "world";
         marker_ball.header.frame_id = "camera_init";
         marker_ball.header.stamp = rclcpp::Clock().now();
-        marker_ball.ns = ns.c_str();
+        marker_ball.ns = ns;
         marker_ball.id = id >= 0 ? id : cnt++;
         marker_ball.action = visualization_msgs::msg::Marker::ADD;
         marker_ball.pose.orientation.w = 1.0;
@@ -329,7 +321,7 @@ namespace rog_map {
         marker_ball.pose.position = p;
         mkr_arr.markers.push_back(marker_ball);
 
-        // add test
+        // add text
         if (print_ns) {
             visualization_msgs::msg::Marker marker;
             // marker.header.frame_id = "world";
@@ -341,8 +333,8 @@ namespace rog_map {
             if (id >= 0) {
                 marker.id = id;
             } else {
-                static int id = 0;
-                marker.id = id++;
+                static int static_id = 0;
+                marker.id = static_id++;
             }
             marker.type = visualization_msgs::msg::Marker::TEXT_VIEW_FACING;
             marker.scale.z = 0.6;
@@ -356,19 +348,21 @@ namespace rog_map {
         }
     }
 
-    static void visualizeBoundingBox(visualization_msgs::msg::MarkerArray &mkrarr,
+    inline void visualizeBoundingBox(visualization_msgs::msg::MarkerArray &mkrarr,
                                      const Vec3f &box_min,
                                      const Vec3f &box_max,
-                                     const string &ns,
+                                     const std::string &ns,
                                      const Color &color,
-                                     const double &size_x = 0.1,
-                                     const double &alpha = 1.0,
-                                     const bool &print_ns = true) {
-        Vec3f size = (box_max - box_min) / 2;
-        Vec3f vis_pos_world = (box_min + box_max) / 2;
+                                     double size_x = 0.1,
+                                     double alpha = 1.0,
+                                     bool print_ns = true) {
+        (void)print_ns; // Suppress unused parameter warning
+        
+        Vec3f size = (box_max - box_min) / 2.0;
+        Vec3f vis_pos_world = (box_min + box_max) / 2.0;
         double width = size.x();
         double length = size.y();
-        double hight = size.z();
+        double height = size.z();
 
         //Publish Bounding box
         int id = 0;
@@ -391,28 +385,28 @@ namespace rog_map {
         //vis_pos_world是目标物的坐标
         p[0].x = vis_pos_world(0) - width;
         p[0].y = vis_pos_world(1) + length;
-        p[0].z = vis_pos_world(2) + hight;
+        p[0].z = vis_pos_world(2) + height;
         p[1].x = vis_pos_world(0) - width;
         p[1].y = vis_pos_world(1) - length;
-        p[1].z = vis_pos_world(2) + hight;
+        p[1].z = vis_pos_world(2) + height;
         p[2].x = vis_pos_world(0) - width;
         p[2].y = vis_pos_world(1) - length;
-        p[2].z = vis_pos_world(2) - hight;
+        p[2].z = vis_pos_world(2) - height;
         p[3].x = vis_pos_world(0) - width;
         p[3].y = vis_pos_world(1) + length;
-        p[3].z = vis_pos_world(2) - hight;
+        p[3].z = vis_pos_world(2) - height;
         p[4].x = vis_pos_world(0) + width;
         p[4].y = vis_pos_world(1) + length;
-        p[4].z = vis_pos_world(2) - hight;
+        p[4].z = vis_pos_world(2) - height;
         p[5].x = vis_pos_world(0) + width;
         p[5].y = vis_pos_world(1) - length;
-        p[5].z = vis_pos_world(2) - hight;
+        p[5].z = vis_pos_world(2) - height;
         p[6].x = vis_pos_world(0) + width;
         p[6].y = vis_pos_world(1) - length;
-        p[6].z = vis_pos_world(2) + hight;
+        p[6].z = vis_pos_world(2) + height;
         p[7].x = vis_pos_world(0) + width;
         p[7].y = vis_pos_world(1) + length;
-        p[7].z = vis_pos_world(2) + hight;
+        p[7].z = vis_pos_world(2) + height;
         //LINE_STRIP类型仅仅将line_strip.points中相邻的两个点相连，如0和1，1和2，2和3
         for (int i = 0; i < 8; i++) {
             line_strip.points.push_back(p[i]);
